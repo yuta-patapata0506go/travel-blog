a652ef3d1777eb5520aad63835a254bf48f7ab7f
Miki <aaa.miki0411@gmail.com>
2024-10-11 21:24:40 +0900

    [UI/navbar] - Partial (only for guest) navbar in UI 	-	Added navbar.blade.php 	-	Added navbar to routes 	-	Added public/css/styles.css 	-	Added public/images/logos/whereToGo_navbar_sample.png 	-	Edited app.blade.php
    	*	There are conflicts in app.blade.php and web.php. They are unresolved.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/navbar', function () {
+    return view('navbar');
+});
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
