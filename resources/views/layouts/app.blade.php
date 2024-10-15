a1fe93fa02e03c3f87541b0d40ca5b029ba95708
Tomomi <ura118@yahoo.co.jp>
2024-10-15 21:00:20 +0900

    [UI/select post form] - Added Select Post Form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,19 +9,45 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <!-- Bootstrap CSS（最新版のみに統一） -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU5p7n9vfqVq1GoY+l1C5ZBJ4C5goa1Bf2Zn1" crossorigin="anonymous">
+    
+    <!-- Bootstrap Datepicker CSS（最新版に統一） -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" rel="stylesheet">
+    
+    <!-- Bootstrap Icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" rel="stylesheet">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- App-specific CSS -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- App Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- jQuery（最新版のみを読み込み） -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap JavaScript（最新版のみを読み込み） -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka74Nf+7Yj/J+gZT0ibfScp4jOv2+6b5lCt6Kt7f/uQ/6cwQLDk5JmQubd9vF5kZ" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap Datepicker JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+ 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'WhereToGo') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -70,11 +96,27 @@
                     </ul>
                 </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+        </nav> --}}
+        <main class="py-4">            
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-11">
+                      @yield('content')
+                    </div>
+               </div>
+            </div>
+        </main>    
+        
+    </div>      
+        {{-- <footer class="text-center py-3">
+            <div class="container d-flex flex-column align-items-center">
+                <p class="mb-0">&copy; 2024 WhereToGo</p>
+                <nav class="footer-nav mt-2">
+                    <a href="#" class="text-white mx-2">Contact</a>
+                    <a href="#" class="text-white mx-2">About</a>
+                </nav>
+            </div>
+        </footer> --}}    
+        </body>
 </html>
+
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/select-post-form', function () {
+    return view('select-post-form');
+});
+
+Route::get('/spot-post-form', function () {
+    return view('spot-post-form');
+});
+
+Route::get('/event-post-form', function () {
+    return view('event-post-form');
+});
+
+Route::get('/tourism-post-form', function () {
+    return view('tourism-post-form');
+});
+
+Route::get('/edit-event-post', function () {
+    return view('edit-event-post');
+});
+
+Route::get('/edit-tourism-post', function () {
+    return view('edit-tourism-post');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
