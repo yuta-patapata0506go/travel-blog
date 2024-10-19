95ce647e52bacb2db6bb4ef4e6c56db8fd4688c2
Mutsumi22 <suisougaku12345@yahoo.co.jp>
2024-10-19 16:13:16 +0900

    [UI/admin/spots/update] [UI/admin/spots/create] [UI/admin/spot_applications/allowCreate]
    Changes
    - add margin betweent forms
    - change button color

--- resources/views/admin/spots/create.blade.php
+++ resources/views/admin/spots/create.blade.php
@@ -1,41 +1,32 @@
-<link rel="stylesheet" href="{{asset('css/admin/spot.css')}}">
+<link rel="stylesheet" href="{{asset('css/admin/form.css')}}">
 @extends('layouts.app')
 
 @section('content')
-
- 
         <h1 class="text-center">Create New Spot</h1>
-
         <form method="POST" action="#" enctype="multipart/form-data">
             @csrf
-
             <!-- Spot Name -->
-            <div class="form-group mb-3">
+            <div class="form-group mb-5">
                 <label for="spot_name">Spot Name:</label>
                 <input type="text" class="form-control" id="spot_name" name="spot_name" placeholder="Spot Name" value="{{ old('spot_name') }}">
             </div>
-
             <!-- Address -->
-            <div class="form-group mb-3">
+            <div class="form-group mb-5">
                 <label for="address">Address:</label>
                 <input type="text" class="form-control" id="address" name="address" placeholder="Address" value="{{ old('address') }}">
             </div>
-
             <!-- Image Upload -->
-            <div class="form-group mb-3">
+            <div class="form-group mb-3 w-70">
                 <label for="image" class="form-label">Image:</label>
                 <img src="{{ asset('images/firework.jpeg') }}" class="img-responsive small-image" alt="Firework Image 1">
                 <img src="{{ asset('images/firework.jpeg') }}" class="img-responsive small-image" alt="Firework Image 1">
                 <img src="{{ asset('images/firework.jpeg') }}" class="img-responsive small-image" alt="Firework Image 1">
-                <img src="{{ asset('images/firework.jpeg') }}" class="img-responsive small-image" alt="Firework Image 1">
-                
-                <input type="file" class="form-control-file d-block my-3 wide-input" id="image" name="image"> 
+                <img src="{{ asset('images/firework.jpeg') }}" class="img-responsive small-image" alt="Firework Image 1">           
+                <input type="file" class="form-control-file d-block my-1 wide-input" id="image" name="image"> 
                 <small class="form-text text-muted ">
                     Acceptable formats: jpeg, jpg, png, gif. Max file size: 10MB.
                 </small>
             </div>
-
-
             <!-- Latitude and Longitude -->
             <div class="form-row mt-5">
                 <div class="form-group">
@@ -47,9 +38,8 @@
                     <input type="number" class="form-control short-input" id="longitude" name="longitude" placeholder="123.456789" value="{{ old('longitude') }}">
                 </div>
             </div>
-
             <!-- Hidden or Visible -->
-            <div class="form-row">
+            <div class="form mt-5">
             <div class="dropdown">
                     <button class="btn btn-sm" data-bs-toggle="dropdown">
                         Visibility
@@ -68,8 +58,8 @@
             </div>
             <!-- Buttons -->
             <div class="form-group text-center">
-                <button type="submit" class="btn-admin">Create</button>
-                <a href="#" class="btn-admin">Cancel</a>
+                <a href="#" class="btn-admin ">Cancel </a>
+                <button type="submit" class="btn-admin ">Create</button>
             </div>
         </form>
 @endsection
