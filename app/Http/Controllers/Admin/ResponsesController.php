5d282b777106309e24f26b2560c120266f127cb9
Miki <aaa.miki0411@gmail.com>
2024-10-29 20:33:57 +0900

    [Backend/admin/reply-form] - in progress - Updated Admin/ResponsesController.php - Added app/Mail/ReplyMail.php
    I'm in progress!!

--- app/Http/Controllers/Admin/ResponsesController.php
+++ app/Http/Controllers/Admin/ResponsesController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Response;
 use App\Models\Inquiry;
 use App\Models\User;
-// use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 
 class ResponsesController extends Controller
 {
@@ -33,6 +34,30 @@ class ResponsesController extends Controller
     }
 
     // store
+    public function store(Request $request, $inquiry_id)
+    {
+        // Validate
+        $request->validate([
+            'body' => 'required|min:1|max:1000',
+        ]);
+
+        // save the reply
+        $this->response->user_id = Auth::user()->id;
+        $this->response->inquiry_id = $inquiry_id;
+        $this->response->body  =   $request->body;
+        $this->response->save();
+
+        // send mail
+        $inquiry = $this->inquiry->findOrFail($inquiry_id);
+        Mail::to($inquiry->user->email)->send(new ReplyMail($this->response->body));
+
+        // make status responsed
+        $inquiry->status = 'responsed';
+        $inquiry->save();
+
+        // redirect
+        return redirect()->route('admin.inquiries.inquiry_details', $inquiry_id); // route
+    }
 
 
 }
