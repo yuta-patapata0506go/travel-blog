9279e472fb91b033d09511fd573f02b7647ef01f
Miki <aaa.miki0411@gmail.com>
2024-11-09 11:54:43 +0900

    [Backend/admin/inquiry-management-2] - Updated tab links in index pages for admin side - Updated categories-index.blade.php - Updated inquiries-index.blade.php - Updated posts-index.blade.php - Updated spot_applications-index.blade.php - Updated spots-index.blade.php - Updated users-index.blade.php

--- resources/views/admin/spots/spots-index.blade.php
+++ resources/views/admin/spots/spots-index.blade.php
@@ -47,7 +47,7 @@
                         <a href="admin-categories-index" class="icon-item">
                             <i class="fa-solid fa-shapes"></i>
                         </a>
-                        <a href="admin-inquiries-index" class="icon-item">
+                        <a href="{{ route('admin.inquiries.index') }}" class="icon-item">
                             <i class="fa-solid fa-address-card"></i>
                         </a>
                         <a href="admin-spot_applications-index" class="icon-item">
--- resources/views/admin/users/users-index.blade.php
+++ resources/views/admin/users/users-index.blade.php
@@ -37,7 +37,7 @@
                                 <a href="admin-categories-index" class="icon-item">
                                     <i class="fa-solid fa-shapes"></i>
                                 </a>
-                                <a href="admin-inquiries-index" class="icon-item">
+                                <a href="{{ route('admin.inquiries.index') }}" class="icon-item">
                                     <i class="fa-solid fa-address-card"></i>
                                 </a>
                                 <a href="admin-spot_applications-index" class="icon-item">
