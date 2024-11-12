2f8d52ff76ea03af7186dbb3d01df914516bf154
Mutsumi22 <suisougaku12345@yahoo.co.jp>
2024-11-12 20:50:14 +0900

    Deleted partial code in Event.php

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,14 +9,5 @@ class Event extends Model
 {
     use HasFactory;
 
-    $events = Event::all();
-
-    // 特定のイベント名を取得
-    foreach ($events as $event) {
-        echo $event->event_name;
-    }
     
-    // 今日のイベントを取得
-    $todayEvents = Event::whereDate('start_date', today())->get();
-
 }
