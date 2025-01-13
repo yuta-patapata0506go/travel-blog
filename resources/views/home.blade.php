44251a6a1be85b63686b7cd9fc01bbc3864618ea
奥畑雄太 <oku.cheko@gmail.com>
2025-01-13 22:19:53 +0900

    change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,7 +61,7 @@
             <div class="image-item">
                <a href="{{ route('display.tourism') }}"> <!-- ツーリズムページへのリンク -->
                   <img src="images/tourism-link.png" alt="Tourism Page">
-                  <div class="overlay-text">Tourism</div>
+                  <div class="overlay-text">Tourism Page</div>
                </a>
             </div>
         </div>            
